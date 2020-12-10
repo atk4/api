be8067638d7c53bebe050925b9badef333d09534
DarkSide <imants.horsts@inbox.lv>
2020-12-10 16:03:16 +0200

    proper hook call

--- examples/Country.php
+++ examples/Country.php
@@ -22,30 +22,36 @@ class Country extends Model
         $this->addField('numcode', ['caption' => 'ISO Numeric Code', 'type' => 'integer', 'required' => true]);
         $this->addField('phonecode', ['caption' => 'Phone Prefix', 'type' => 'integer']);
 
-        $this->onHook('beforeSave', function ($m) {
-            if (!$m['sys_name']) {
-                $m['sys_name'] = strtoupper($m['name']);
+        $this->onHook(
+            Model::HOOK_BEFORE_SAVE,
+            function ($m) {
+                if (!$m['sys_name']) {
+                    $m['sys_name'] = strtoupper($m['name']);
+                }
             }
-        });
+        );
 
-        $this->onHook('validate', function (Model $m) {
-            $errors = [];
+        $this->onHook(
+            Model::HOOK_VALIDATE,
+            function (Model $m) {
+                $errors = [];
 
-            if (strlen($m['iso']) !== 2) {
-                $errors['iso'] = 'Must be exactly 2 characters';
+                if (strlen($m['iso']) !== 2) {
+                    $errors['iso'] = 'Must be exactly 2 characters';
+                }
+
+                if (strlen($m['iso3']) !== 3) {
+                    $errors['iso3'] = 'Must be exactly 3 characters';
+                }
+
+                // look if name is unique
+                $c = (clone $m)->unload()->tryLoadBy('name', $m['name']);
+                if ($c->loaded() && $c->id !== $m->id) {
+                    $errors['name'] = 'Country name must be unique';
+                }
+
+                return $errors;
             }
-
-            if (strlen($m['iso3']) !== 3) {
-                $errors['iso3'] = 'Must be exactly 3 characters';
-            }
-
-            // look if name is unique
-            $c = (clone $m)->unload()->tryLoadBy('name', $m['name']);
-            if ($c->loaded() && $c->id !== $m->id) {
-                $errors['name'] = 'Country name must be unique';
-            }
-
-            return $errors;
-        });
+        );
     }
 }
