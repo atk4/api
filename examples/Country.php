24e5e433ebe566c9acf9c43acb2d5e55e9751ebd
DarkSide <imants.horsts@inbox.lv>
2020-12-10 15:54:58 +0200

    cs

---
+++ examples/Country.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+require_once '../vendor/autoload.php';
+
+use Atk4\Data\Model;
+
+class Country extends Model
+{
+    public $table = 'country';
+
+    protected function init(): void
+    {
+        parent::init();
+
+        $this->addField('name', ['actual' => 'nicename', 'required' => true, 'type' => 'string']);
+        $this->addField('sys_name', ['actual' => 'name', 'system' => true]);
+
+        $this->addField('iso', ['caption' => 'ISO', 'required' => true, 'type' => 'string']);
+        $this->addField('iso3', ['caption' => 'ISO3', 'required' => true, 'type' => 'string']);
+        $this->addField('numcode', ['caption' => 'ISO Numeric Code', 'type' => 'integer', 'required' => true]);
+        $this->addField('phonecode', ['caption' => 'Phone Prefix', 'type' => 'integer']);
+
+        $this->onHook('beforeSave', function ($m) {
+            if (!$m['sys_name']) {
+                $m['sys_name'] = strtoupper($m['name']);
+            }
+        });
+
+        $this->onHook('validate', function (Model $m) {
+            $errors = [];
+
+            if (strlen($m['iso']) !== 2) {
+                $errors['iso'] = 'Must be exactly 2 characters';
+            }
+
+            if (strlen($m['iso3']) !== 3) {
+                $errors['iso3'] = 'Must be exactly 3 characters';
+            }
+
+            // look if name is unique
+            $c = (clone $m)->unload()->tryLoadBy('name', $m['name']);
+            if ($c->loaded() && $c->id !== $m->id) {
+                $errors['name'] = 'Country name must be unique';
+            }
+
+            return $errors;
+        });
+    }
+}
--- examples/test.php
+++ examples/test.php
@@ -2,57 +2,16 @@
 
 declare(strict_types=1);
 
-use Atk4\Data\Model;
+require_once '../vendor/autoload.php';
+require_once 'Country.php';
 
-include '../vendor/autoload.php';
+use Atk4\Api\Api;
+use Atk4\Data\Persistence;
 
-$api = new \Atk4\Api\Api();
+$api = new Api();
 
-class Country extends \Atk4\Data\Model
-{
-    public $table = 'country';
-
-    protected function init(): void
-    {
-        parent::init();
-
-        $this->addField('name', ['actual' => 'nicename', 'required' => true, 'type' => 'string']);
-        $this->addField('sys_name', ['actual' => 'name', 'system' => true]);
-
-        $this->addField('iso', ['caption' => 'ISO', 'required' => true, 'type' => 'string']);
-        $this->addField('iso3', ['caption' => 'ISO3', 'required' => true, 'type' => 'string']);
-        $this->addField('numcode', ['caption' => 'ISO Numeric Code', 'type' => 'integer', 'required' => true]);
-        $this->addField('phonecode', ['caption' => 'Phone Prefix', 'type' => 'integer']);
-
-        $this->onHook('beforeSave', function ($m) {
-            if (!$m['sys_name']) {
-                $m['sys_name'] = strtoupper($m['name']);
-            }
-        });
-
-        $this->onHook('validate', function (Model $m) {
-            $errors = [];
-
-            if (strlen($m['iso']) !== 2) {
-                $errors['iso'] = 'Must be exactly 2 characters';
-            }
-
-            if (strlen($m['iso3']) !== 3) {
-                $errors['iso3'] = 'Must be exactly 3 characters';
-            }
-
-            // look if name is unique
-            $c = (clone $m)->unload()->tryLoadBy('name', $m['name']);
-            if ($c->loaded() && $c->id !== $m->id) {
-                $errors['name'] = 'Country name must be unique';
-            }
-
-            return $errors;
-        });
-    }
-}
 session_start();
-$db = new \Atk4\Data\Persistence\Sql('mysql:dbname=atk4;host=localhost', 'root', '');
+$db = new Persistence\Sql('mysql:dbname=atk4;host=localhost', 'root', '');
 
 $api->get('/ping/', function () {
     return 'Hello, World';
